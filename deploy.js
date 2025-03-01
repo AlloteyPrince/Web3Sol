91608312765a8084237a1f62605e8a97cd73b208
Prince <oscarprince2020@gmail.com>
2025-03-01 13:34:03 -0800

    third commit

--- deploy.js
+++ deploy.js
@@ -1,5 +1,6 @@
 const ethers = require("ethers")
 const fs = require("fx-extra")
+require("dotenv").config;
 
 async function main(){
  const provider = new ethers.providers.jsonRpcProvider("http://0.0.0.0:8545");
@@ -9,7 +10,21 @@ async function main(){
 
  const contractFactory = new ethers.ContractFactory(abi,binary,wallet);
  console.log("Deploying, please wait...");
- 
+ const contract = await contractFactory.deploy();
+ const deploymentReciept =await contract.deploymentTransaction.wait(1);
+ console.log("Here is the deployment transaction")
+ console.log(deploymentReciept);
+
+
+ const tx = {
+    nonce: 5,
+    gasPrice: 20000000000,
+    gasLight: 1000000,
+    to: null,
+    value: 0,
+    data: "0x94749374j4749473739373fnkbz74nf84b292bf82bf82fsj8994784h4749dhd7399374j283y492023b74",
+    chainId: 1337
+ }
 }
 
 main()
