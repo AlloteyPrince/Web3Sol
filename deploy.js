454f3d774b5e7fc60263ecd4e7cefa4923df475c
Prince <oscarprince2020@gmail.com>
2025-03-01 13:02:52 -0800

    third commit

--- deploy.js
+++ deploy.js
@@ -1,9 +1,15 @@
 const ethers = require("ethers")
-
+const fs = require("fx-extra")
 
 async function main(){
  const provider = new ethers.providers.jsonRpcProvider("http://0.0.0.0:8545");
- const wallet = new ethers.wallet("7dbe329dbf.... from the the ganache site ")
+ const wallet = new ethers.wallet("7dbe329dbf.... from the the ganache site ");
+ const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi");
+ const binary = fs.readFileSync("./SimpleStorage_sol_Simplestorage.bin", "utf8");
+
+ const contractFactory = new ethers.ContractFactory(abi,binary,wallet);
+ console.log("Deploying, please wait...");
+ 
 }
 
 main()
